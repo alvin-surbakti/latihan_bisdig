57454a75d9b016cbaaa06b7e6475188f0a483328
alvin-surbakti <alvinajaaa416@gmail.com>
2025-09-10 14:38:10 +0700

    belajar javascript

---
+++ belajar.js
@@ -0,0 +1,11 @@
+window.onload = function() {
+    alert('Selamat datang di website Muhammad Alvin!');
+};
+confirm("Apakah Anda ingin melanjutkan?");
+prompt("Saya Belajar:");
+Date(10)
+parseInt("200");
+parseFloat("99.99");
+setTimeout(function() {
+    alert("Ini adalah pesan setelah 2 detik");
+}, 2000);
